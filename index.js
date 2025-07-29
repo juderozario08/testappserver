19eb4b006e8d43c4b5dff606646afcf95c6d5723
Jude Rozario <juderzro08@gmail.com>
2025-07-29 16:00:57 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = 3000;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
